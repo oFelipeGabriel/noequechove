0ef8e7bcd4b663bfc7ead4abb3fc022f2fdc2720
Felipe Faria <felipefaria@brq.com>
2022-04-13 11:09:04 -0300

    Corrigido serve.js

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(path.join(__dirname, 'dist/noequechove,')))
 
-var port = process.env.PORT || 5000
-app.listen(port)
+var port = process.env.PORT || 8080
+// app.listen(port)
+// console.log('server started ' + port)
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+
+    '/dist/noequechove/index.html'));
+});
+app.listen(port || 8080);
 console.log('server started ' + port)
